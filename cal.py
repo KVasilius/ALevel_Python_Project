bb5c8d3aad795d007b07e853912f08ddaa595d5c
KVasilius <57482548+KVasilius@users.noreply.github.com>
2021-11-07 18:48:15 +0000

    Delete cal.py

--- cal.py
+++
@@ -1,5 +0,0 @@
-def add(x,y): return x + y
-def sub(x, y): return x - y
-userchoice, x, y = input("mode: "), int(input("num 1: ")), int(input("num 2: "))
-if userchoice == "add": print(add(x,y))
-elif userchoice == "sub": print(sub(x,y))
